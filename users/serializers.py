84f6ab0e0639ec1948214a1369b97b299c55ac9d
JaumLA <joao.l.alvarez@gmail.com>
2026-03-29 14:04:10 -0300

    Using nested serializer for tasks ordering

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
 from tasks.models import Task
+from tasks.serializers import TaskSerializer
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-  tasks = serializers.PrimaryKeyRelatedField(many=True, queryset=Task.objects.all())
-
+  tasks = TaskSerializer(many=True)
   class Meta:
     model = User
     fields = ["id", "username", "tasks"]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Prefetch
 
 from users.serializers import UserSerializer
 from users.models import User
+from tasks.models import Task
 
 class UserViewSet(ModelViewSet):
-  queryset = User.objects.all().prefetch_related("tasks")
+  queryset = User.objects.all().prefetch_related(
+    Prefetch('tasks', queryset=Task.objects.order_by('init_time'))
+  )
   serializer_class = UserSerializer
   permission_classes = [IsAuthenticated]
-
-  def get_queryset(self):
-    if self.request.user.is_superuser:
-      return User.objects.all().prefetch_related("tasks")
-
-    return User.objects.filter(id=self.request.user.id).prefetch_related("tasks")
